7ebb3c14eb17aaa9bdd2e1434e36afd659de6b3a
tiansl960105 <tiansl960105@gmail.com>
2019-11-06 00:40:56 -0500

    Site updated: 2019-11-06 00:40:55

--- dist/music.js
+++ dist/music.js
@@ -5,18 +5,6 @@ const ap = new APlayer({
     theme: '#FADFA3',
     audio: [
       {
-        name: "旋木",
-        artist: '王菲',
-        url: 'http://www.ytmp3.cn/down/58260.mp3',
-        cover: 'https://imgur.com/QE2JOlx.jpg',
-      },
-      {
-        name: 'Bad guy',
-        artist: 'Billie',
-        url: 'http://www.ytmp3.cn/down/73744.mp3',
-        cover: 'https://imgur.com/QE2JOlx.jpg',
-      },
-      {
         name: 'Breaking My Heart',
         artist: 'Lana',
         url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_22810058&response=res&type=convert_url&.mp3',
